1ec19c07d587b64ba00b42df70afea82cb14a93f
ryuk-jayant <jayantdubey2003@gmail.com>
2023-12-08 19:29:43 +0530

    Linked Encode with new Encryption flow

--- encode.py
+++ encode.py
@@ -17,20 +17,22 @@ pwd = "C$_P₹oj#cT"
 # Encoding function for the cipher
 def encode(string, key=pwd):
     enc = []
-    for i in range(len(string)):
-        key_c = key[i % len(key)]
-        # ord() gives the respective ascii value
-        enc_c = chr(ord(string[i]) + ord(key_c) % 256)
-        enc.append(enc_c)
-    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+    # for i in range(len(string)):
+    #     key_c = key[i % len(key)]
+    #     # ord() gives the respective ascii value
+    #     enc_c = chr(ord(string[i]) + ord(key_c) % 256)
+    #     enc.append(enc_c)
+    # return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+    return encrypt(string, key)
 
 # Function to Decode
 def decode(string, key=pwd):
-    dec = []
-    # utf-8 to avoid character mapping errors
-    string = base64.urlsafe_b64decode(string).decode()
-    for i in range(len(string)):
-        key_c = key[i % len(key)]
-        dec_c = chr((256 + ord(string[i]) - ord(key_c)) % 256)
-        dec.append(dec_c)
-    return "".join(dec)
+    # dec = []
+    # # utf-8 to avoid character mapping errors
+    # string = base64.urlsafe_b64decode(string).decode()
+    # for i in range(len(string)):
+    #     key_c = key[i % len(key)]
+    #     dec_c = chr((256 + ord(string[i]) - ord(key_c)) % 256)
+    #     dec.append(dec_c)
+    # return "".join(dec)
+    return decrypt(string,key)
